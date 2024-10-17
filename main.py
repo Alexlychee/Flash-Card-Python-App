ed9067e2aa5ad640f3153e822946184f438d41cb
Alex Ly <70249534+Alexlychee@users.noreply.github.com>
2024-10-16 18:14:24 -0700

    added the window

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+############################# USER INTERFACE #############################
+window = Tk()
+window.title("Flashy")
+window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
+
+window.mainloop()
+
 
