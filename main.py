6b2263809f574fabd93d014a16a0e3e4d6367e45
Alex Ly <70249534+Alexlychee@users.noreply.github.com>
2024-10-16 18:21:48 -0700

    added two buttons unknown and known buttons

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
 
@@ -14,7 +15,16 @@ canvas.create_image(400, 263, image=card_front)
 canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(row=0, column=1, columnspan=2)
+canvas.grid(row=0, column=0, columnspan=2)
+
+# Buttons
+cross_image = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=cross_image, highlightthickness=0)
+wrong_button.grid(row=1, column=0)
+
+check_image = PhotoImage(file="images/right.png")
+right_button = Button(image=check_image, highlightthickness=0)
+right_button.grid(row=1, column=1)
 
 window.mainloop()
 
