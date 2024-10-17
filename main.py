ba58241337a242f82bd18ab70f402e6935c51ee9
Alex Ly <70249534+Alexlychee@users.noreply.github.com>
2024-10-16 18:17:08 -0700

    added flashcard UI

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ window = Tk()
 window.title("Flashy")
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
+# Flash Card
+canvas = Canvas(width=800, height=526)
+card_front = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=1, columnspan=2)
+
 window.mainloop()
 
 
